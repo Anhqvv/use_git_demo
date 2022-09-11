4174879c0133050cbb8ce19c9ad461cebd957893
Anhqvv <anhqvv2022@gmail.com>
2022-09-11 16:11:52 +0700

    them phan button vao

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const app = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+app.propTypes = {};
+
+export default app;
